18dcbe9e3326414f67ac97e4e5b4d970ce4fa670
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-08-19 12:48:34 +0100

    Add files via upload

--- companydirectory/libs/js/script.js
+++ companydirectory/libs/js/script.js
@@ -182,7 +182,7 @@
 */var table = $('#myTable').DataTable({
   
 ajax: {
-  url:  "https://companypro.herokuapp.com/companydirectory/libs/php/getall.php",
+  url:  "/companydirectory/libs/php/getall.php",
   dataSrc: 'data',
   cache: true,
   responsive: true,
@@ -214,7 +214,7 @@ columns: [{data: 'firstName'},
       })
 var departmentTable = $('#departmentTable').DataTable({
 ajax: {
-  url:  "https://companypro.herokuapp.com/companydirectory/libs/php/getallDepartments.php",
+  url:  "/companydirectory/libs/php/getallDepartments.php",
   dataSrc: 'data',
   cache: true,
   responsive: true,
@@ -253,7 +253,7 @@ columns: [{data: 'name'},
       var locationTableS = $('#locationTable').DataTable({
   
         ajax: {
-          url:  "https://companypro.herokuapp.com/companydirectory/libs/php/getLocation.php",
+          url:  "/companydirectory/libs/php/getLocation.php",
           dataSrc: 'data',
           cache: true,
           responsive: true,
