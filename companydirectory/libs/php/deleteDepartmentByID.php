37eef9befc0b43bb7c1ee57885df37e97b9bde79
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-09-01 15:56:39 +0100

    Update deleteDepartmentByID.php

--- companydirectory/libs/php/deleteDepartmentByID.php
+++ companydirectory/libs/php/deleteDepartmentByID.php
@@ -77,10 +77,10 @@
 		mysqli_close($conn);
 
 		echo json_encode($output); 
-
-	//	 header("location: ../../../index.php");
 	
-      // 	exit();
+		header("location: ../../../index.php");
+	
+      	exit();
 	}
 
 
