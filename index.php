c1412766f83381bf753d6b31e5761c2176cd58c6
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-09-03 13:37:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+   
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   
     <link rel="stylesheet" href="companydirectory/libs/css/styles.css" type='text/css'>
-    <script src="https://cdn.jsdelivr.net/npm/jquery.fancytable/dist/fancyTable.min.js"></script>
+   
     
  
     <style>
