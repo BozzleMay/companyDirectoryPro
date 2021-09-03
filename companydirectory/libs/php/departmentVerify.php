60ff5842167ec13d3f58910298bc5e80d51d42ad
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-09-03 10:57:18 +0100

    Add files via upload

---
+++ companydirectory/libs/php/departmentVerify.php
@@ -0,0 +1,43 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$id = "";
+
+    $id = $_REQUEST["val"];
+    
+    
+        $sql = "SELECT name from department WHERE NOT EXISTS (SELECT * FROM personnel WHERE personnel.departmentID = department.id) AND id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+           $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+                if(mysqli_num_rows($result) != 1){
+                    
+                }
+		
+            }
+	
+		
+    
+              
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($link);
+
+        echo json_encode($result); 
+    
+
+
+?>
