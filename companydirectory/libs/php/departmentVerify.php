03906789630aaf8fb59d1fca2817f17f1c037739
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-09-03 11:00:18 +0100

    Add files via upload

--- companydirectory/libs/php/departmentVerify.php
+++ companydirectory/libs/php/departmentVerify.php
@@ -20,7 +20,7 @@ $id = "";
             if(mysqli_stmt_execute($stmt)){
                 $result = mysqli_stmt_get_result($stmt);
                 if(mysqli_num_rows($result) != 1){
-                    
+                    echo json_encode($param_id); 
                 }
 		
             }
